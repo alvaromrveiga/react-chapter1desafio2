54d4f6e67e4bec635e19b77431803f97276f3498
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-02-10 13:12:05 -0300

    useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Content } from "./components/Content";
 import { SideBar } from "./components/SideBar";
 
@@ -9,9 +9,9 @@ import "./styles/sidebar.scss";
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  function handleClickButton(id: number) {
+  const handleClickButton = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  }, []);
 
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
